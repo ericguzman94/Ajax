7466658b01a6579e0bfdb8407fd4f8908d12d053
Eric Guzman <striderkills@gmail.com>
2019-04-19 11:29:47 -0700

    loaded from api

--- cpsc349-master/week13/comments.js
+++ cpsc349-master/week13/comments.js
@@ -3,15 +3,56 @@
 
   var $titles = $['#title'];
 
+  var tit = '[data-posts="title"]';
+  var bod = '[data-posts="body"]';
+  var com = '[data-comments="body"]';
+  var name = '[data-comments="name"]';
+  var email = '[data-comments="email" ]';
+  var reff = '[href="mailto:Presley.Mueller@myrl.com"]';
+
   $.ajax({
-    type: "GET",
-    url: "https://jsonplaceholder.typicode.com/posts",
+    type: 'GET',
+    url: 'https://jsonplaceholder.typicode.com/posts',
     success: function (data) {
-      $.each(data, function (indexInArray, valueOfElement) { 
-         $titles.append(valueOfElement.);
+      console.log("success", data)
+      $.each(data, function (i, item) { 
+        console.log(item.userId);
+        if (item.userId == 1 && item.id == 1 ) {
+          $(tit).append(item.title);
+          $(bod).append(item.body);
+        }
+        else if (item.userId == 2 && item.id == 2) {
+          $(tit).append(item.title);
+          $(bod).append(item.body);
+        } 
       });
     }
   });
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://jsonplaceholder.typicode.com/comments?postId=1',
+    success: function (data) {
+      console.log("success", data)
+      $.each(data, function (i, item) { 
+        console.log(item.userId);
+        if (item.postId == 1 && item.id == 1 ) {
+          $(com).append(item.body);
+          $(name).append(item.name);
+          $(email).append(item.email);
+        }
+        else if (item.userId == 2 && item.id == 2) {
+          $(com).append(item.body);
+        } 
+      });
+    }
+  });
+
+
+  $.ajax({
+    type: 'GET',
+    
+  })
 
 
 
